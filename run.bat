aa95495268c7cc0bf35b7527d7b29eb446d8b185
Suzusiro <64842311+suzusiro-0w0@users.noreply.github.com>
2025-08-13 02:06:28 +0900

    プレイヤー人数を名称に表示しバッチをShift-JIS化

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
+@echo off
+REM zL
 call venv\Scripts\activate
+REM {bgN
 python "MCS-DiscordRPC.py"
 pause
---
+++ setup.bat
@@ -0,0 +1,5 @@
+@echo off
+REM KvȃCuCXg[ZbgAbvob`
+python -m venv venv
+call venv\Scripts\activate
+pip install -r requirements.txt
