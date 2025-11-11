4d52c466470a46ee2d3c48d6885cb42d55dcbcb1
Suzusiro <azuel.k9@gmail.com>
2025-11-12 01:34:32 +0900

    move file

--- setup.bat
+++
@@ -1,6 +0,0 @@
-
-@echo off
-REM KvȃCuCXg[ZbgAbvob`
-python -m venv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
---
+++ setup/setup.bat
@@ -0,0 +1,6 @@
+
+@echo off
+REM KvȃCuCXg[ZbgAbvob`
+python -m venv venv
+call venv\Scripts\activate
+pip install -r requirements.txt
